c9fa969e6bbfc2c2c00278751b59d2500b44db24
shanehuang <borderun1010@gmail.com>
2017-04-07 01:17:00 +0800

    implement posts pagination with will_paginatie

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
         @post.user = current_user
 
         if @post.save
-            redirect_to groups_path(@group)
+            redirect_to group_path(@group)
         else
             render :new
         end
